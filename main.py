00989b935f167ed685b44fbec1b28edfc57adfac
Ahmet Muratdagi <ahmetmuratdagi7@hotmail.com>
2022-09-29 13:12:20 +0200

    introduction + typewriter Eff

---
+++ main.py
@@ -0,0 +1,56 @@
+#imports
+import sys
+from time import sleep
+
+def typewriterEff(words):
+    for character in words:
+        sleep(0.05)
+        sys.stdout.write(character)
+        sys.stdout.flush()
+
+typewriterEff("""It is a cold, misty day in the Himalaya Mountains and you are trying to hike to the peak of Mount Everest, the highest mountain in the world.
+You are with your companions Peter and Maxwell.""")
+
+typewriterEff("""
+
+Peter: 'Wait.. """)
+sleep(0.75)
+typewriterEff("""what is that sound from the distance?'""")
+sleep(0.5)
+
+typewriterEff("""
+
+Maxwell: 'I have no idea! But it does not sound good.'""")
+sleep(0.5)
+
+typewriterEff("""
+
+You're looking up in the distance and you see... """)
+sleep(1)
+
+print("AN AVALANCHE!")
+sleep(0.5)
+
+typewriterEff("""
+Suddenly everything turns white... """)
+sleep(0.5)
+typewriterEff("""and you only hear Peter and Maxwell yelling!""")
+sleep(0.5)
+
+typewriterEff("""
+
+Then..""")
+sleep(1)
+typewriterEff(""" everything turns dark for a few seconds...""")
+sleep(0.5)
+
+typewriterEff("""
+
+You open your eyes and see a bright light, the sun is shining and you realise that you've been out cold for a very long time.""")
+sleep(0.5)
+
+typewriterEff("""
+Your mouth is dry and you are hungry.
+The backpack you took with you is in front of you, it survived the avalanche.
+Maybe there is something in your backpack which might be useful.
+""")
