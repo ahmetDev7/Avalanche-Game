ffb387f953d7023e76d11dc77969b6ad1f08d507
Koray53 <korayavcips4@gmail.com>
2022-10-01 19:16:38 +0200

    Survival book lowercase

--- Survivalbook.py
+++ Survivalbook.py
@@ -8,6 +8,10 @@ You must avoid food from plants because
 these can be poisonous, rather you should try
 to hunt herbivorous animals like deer or cattle
 or aquatic animals like fish.""")
+    if input().lower() == "next page":
+        SurvivalBookCh2()
+    elif input().lower() == "previous page":
+        print("There are no previous pages")
 
 def SurvivalBookCh2():
     print("""'Chapter 2. Find shelter'
@@ -19,6 +23,10 @@ Caves are very handy for cooking food,
 because wildlife might get attracted if you do
 it openly in the wild. Also there is no wind
 inside so your fire will stay put.""")
+    if input().lower() == "next page":
+        SurvivalBookCh3()
+    elif input().lower() == "previous page":
+        OpenSurvivalBook()
 
 def SurvivalBookCh3():
     print("""'Chapter 3. Resources'
@@ -31,6 +39,10 @@ Also tools like a fishing rod might come in handy if
 there is a running water source nearby, with this you
 can catch aquatic animals to consume and stay free
 from hunger.""")
+    if input().lower() == "next page":
+        SurvivalBookCh4()
+    elif input().lower() == "previous page":
+        SurvivalBookCh2()
 
 def SurvivalBookCh4():
     print("""'Chapter 4. Start a fire'
@@ -44,29 +56,38 @@ Wood: You can get this by cutting a tree or look around
 if you can find a dry log.
 When you have all of these materials you have the
 tools to start a fire, now you only need a fire source.""")
+    if input().lower() == "next page":
+        SurvivalBookCh5()
+    elif input().lower() == "previous page":
+        SurvivalBookCh3()
 
 def SurvivalBookCh5():
     print("""'Chapter 5. Wild animals'
 Be careful of wild animals. Wild animals can be
 dangerous but you can tame them by using food like
 berries.""")
+    if input().lower() == "next page":
+        print("This is the end of the book.")
+    elif input().lower() == "previous page":
+        SurvivalBookCh4()
+
 
 Bookopened = False
 
-if input() == "OpenSurvivalBook":
+if input().lower() == "open survivalbook":
     OpenSurvivalBook()
     Bookopened = True
 
-if input() == "Chapter 2" and Bookopened == True:
+if input().lower() == "chapter 2" and Bookopened == True:
     SurvivalBookCh2()
 
-if input() == "Chapter 3" and Bookopened == True:
+if input().lower() == "chapter 3" and Bookopened == True:
     SurvivalBookCh3()
 
-if input() == "Chapter 4" and Bookopened == True:
+if input().lower() == "chapter 4" and Bookopened == True:
     SurvivalBookCh4()
 
-if input() == "Chapter 5" and Bookopened == True:
+if input().lower() == "chapter 5" and Bookopened == True:
     SurvivalBookCh5()
 
 #hello
