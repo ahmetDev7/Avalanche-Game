0c8b699c5b7f59bc6373d0ec83b44b2a5accd8e7
ahmetmuratdagi7 <ahmetmuratdagi7@hotmail.com>
2022-10-03 20:15:10 +0200

    comments deleted

--- main.py
+++ main.py
@@ -4,50 +4,50 @@ from pyfiglet import figlet_format
 import os
 from locations import *
 
-# typewriterEff("""It is a cold, misty day in the Himalaya Mountains and you are trying to hike to the peak of Mount Everest, the highest mountain in the world.
-# You are with your companions Peter and Maxwell.""")
+typewriterEff("""It is a cold, misty day in the Himalaya Mountains and you are trying to hike to the peak of Mount Everest, the highest mountain in the world.
+You are with your companions Peter and Maxwell.""")
 
-# typewriterEff("""
+typewriterEff("""
 
-# Peter: 'Wait.. """)
-# sleep(0.75)
-# typewriterEff("""what is that sound from the distance?'""")
-# sleep(0.5)
+Peter: 'Wait.. """)
+sleep(0.75)
+typewriterEff("""what is that sound from the distance?'""")
+sleep(0.5)
 
-# typewriterEff("""
+typewriterEff("""
 
-# Maxwell: 'I have no idea! But it does not sound good.'""")
-# sleep(0.5)
+Maxwell: 'I have no idea! But it does not sound good.'""")
+sleep(0.5)
 
-# typewriterEff("""
+typewriterEff("""
 
-# You're looking up in the distance and you see... """)
-# sleep(1)
+You're looking up in the distance and you see... """)
+sleep(1)
 
-# print("AN AVALANCHE!")
-# sleep(0.5)
+print("AN AVALANCHE!")
+sleep(0.5)
 
-# typewriterEff("""
-# Suddenly everything turns white... """)
-# sleep(0.5)
-# typewriterEff("""and you only hear Peter and Maxwell yelling!""")
-# sleep(0.5)
+typewriterEff("""
+Suddenly everything turns white... """)
+sleep(0.5)
+typewriterEff("""and you only hear Peter and Maxwell yelling!""")
+sleep(0.5)
 
-# typewriterEff("""
+typewriterEff("""
 
-# Then..""")
-# sleep(1)
-# typewriterEff(""" everything turns dark for a few seconds...""")
-# sleep(0.5)
+Then..""")
+sleep(1)
+typewriterEff(""" everything turns dark for a few seconds...""")
+sleep(0.5)
 
-# typewriterEff("""
+typewriterEff("""
 
-# You open your eyes and see a bright light, the sun is shining and you realise that you've been out cold for a very long time.""")
-# sleep(0.5)
+You open your eyes and see a bright light, the sun is shining and you realise that you've been out cold for a very long time.""")
+sleep(0.5)
 
-# os.system('cls')
+os.system('cls')
 
-# typewriterEffTitle(figlet_format("The Avalanche", font="big"))
+typewriterEffTitle(figlet_format("The Avalanche", font="big"))
 
 typewriterEff("""
 Your mouth is dry and you are hungry.
